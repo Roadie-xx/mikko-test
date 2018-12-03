0eed885e9b370af62a3aa23f0c582ebb8282113a
Roadie-xx <robbyvz@hotmail.com>
2018-12-03 17:15:15 +0100

    Add files via upload

---
+++ Tests/src/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+
+include_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'Scripts' . DIRECTORY_SEPARATOR . 'App.php';
+include_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'Scripts' . DIRECTORY_SEPARATOR . 'FileHelper.php';
+include_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'Scripts' . DIRECTORY_SEPARATOR . 'PaymentDate.php';
+include_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'Scripts' . DIRECTORY_SEPARATOR . 'Settings.php';
